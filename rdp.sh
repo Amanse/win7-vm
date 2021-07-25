e153cea69a22f507f3e74d36f54cc72b558dd3c3
Aman Setia <amansetiarjp@gmail.com>
2021-07-25 15:04:38 +0530

    idk

--- rdp.sh
+++ rdp.sh
@@ -1,3 +1,4 @@
 wget -O crunge.sh https://raw.githubusercontent.com/kmille36/Linux-to-Windows-with-QEMU/master/Katacoda_Windows7SuperLite.sh
 chmod +x crunge.sh
 ./crunge.sh
+#https://mega.nz/folder/2voGwJaJ#6AI34NyvCovU63ccZsi9pQ/folder/m3AAVDaR
